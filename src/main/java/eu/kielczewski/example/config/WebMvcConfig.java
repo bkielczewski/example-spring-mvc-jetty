42e37d912dc25722c5a48630452ca8877eab1579
bkielczewski <bartosz@kielczewski.eu>
2013-11-04 23:44:01 +0000

    Deleted DefaultServletHandlerConfigurer config, which is not needed in this example.

--- src/main/java/eu/kielczewski/example/config/WebMvcConfig.java
+++ src/main/java/eu/kielczewski/example/config/WebMvcConfig.java
@@ -1,17 +1,10 @@
 package eu.kielczewski.example.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-
 }
