fa979ca74e02796166119df9770f55a34a81f35f
bkielczewski <bartosz@kielczewski.eu>
2013-11-06 20:22:44 +0000

    Added constants for readability

--- src/main/java/eu/kielczewski/example/initializer/AppInitializer.java
+++ src/main/java/eu/kielczewski/example/initializer/AppInitializer.java
@@ -13,18 +13,21 @@ import javax.servlet.ServletRegistration;
 @SuppressWarnings("UnusedDeclaration")
 public class AppInitializer implements WebApplicationInitializer {
 
+    public static final String CONFIG_LOCATION = "eu.kielczewski.example.config";
+    public static final String MAPPING_URL = "/*";
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         WebApplicationContext context = getContext();
         servletContext.addListener(new ContextLoaderListener(context));
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/*");
+        dispatcher.addMapping(MAPPING_URL);
     }
 
     private AnnotationConfigWebApplicationContext getContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.setConfigLocation("eu.kielczewski.example.config");
+        context.setConfigLocation(CONFIG_LOCATION);
         return context;
     }
 
